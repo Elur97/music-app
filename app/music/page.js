a8160281cf0996898ee0bfc7f7131d5683902a8c
Ryo <urase0907@gmail.com>
2025-04-03 13:48:55 +0900

    Update page.js

--- app/music/page.js
+++ app/music/page.js
@@ -160,7 +160,7 @@ export default function MusicPage() {
       {/* ホームページに戻るボタン */}
       <button
   onClick={() => router.push("/")}
-  className="fixed  md:mr-0 mr-30  bottom-4 right-4 bg-blue-500 hover:bg-blue-800 text-white justify-center items-center px-4 py-2 rounded"
+  className="fixed  md:mr-0 mr-28  bottom-4 right-4 bg-blue-500 hover:bg-blue-800 text-white justify-center items-center px-4 py-2 rounded"
 >
   ホームに戻る
 </button>
