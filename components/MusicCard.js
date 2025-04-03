daa2790cefa7ce9ffc9eb215871bc3a7be709a34
sekiryo-g-3 <urase0907@gmail.com>
2025-04-03 11:57:19 +0900

    inn

--- components/MusicCard.js
+++ components/MusicCard.js
@@ -17,14 +17,14 @@ export default function MusicCard({ bookmarkedPosts }) {  // propsでbookmarkedP
   };
 
   return (
-    <div className="p-4 md:space-y-5 md:w-380 w-209">
-      <h2 className="md:text-4xl md:font-bold md:ml-0  text-black">ブックマーク一覧</h2>
+    <div className="p-4 md:space-y-5 md:w-380 w-85 cd:mr-0 mr-14">
+      <h2 className="md:text-4xl text-2xl font-bold md:ml-0  text-black">ブックマーク一覧</h2>
         <div className=" md:space-x-6  ">
         {bookmarkedPosts && bookmarkedPosts.length > 0 ? (
           bookmarkedPosts.map((post) => (
             <div
               key={post.id}
-              className="flex flex-col md:w-370 md w-100  xl:ml-0 ml-50  border p-4 rounded-md bg-white "
+              className="flex flex-col md:w-370  w-90 xl:ml-0   border p-4 rounded-md bg-white "
             >
               <h4 className="md:ml-0  font-bold ">{post.title}</h4>
               <p className="md:w-370 ">{post.comment}</p>
@@ -60,7 +60,7 @@ export default function MusicCard({ bookmarkedPosts }) {  // propsでbookmarkedP
       </div>
       <button
         onClick={() => router.push("/")}
-        className="md:ml-170 ml-86 bg-blue-500 hover:bg-blue-800 text-white px-4 py-2 rounded mt-4"
+        className="md:ml-170 ml-33 bg-blue-500 hover:bg-blue-800 text-white px-4 py-2 rounded mt-4"
       >
         ホームに戻る
       </button>
