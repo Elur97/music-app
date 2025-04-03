6dd1fda52ec9c9ee1e154c7c5596726c8ce9ea87
Ryo <urase0907@gmail.com>
2025-04-03 13:44:10 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
 
         {/* ヘッダー (横幅いっぱいに広げる) */}
         <header className="bg-gray-800 w-full p-14 px-14 text-white text-center">
-          <h1 className="font-serif text-5xl sm:text-6xl font-bold">Sharing Memories</h1>
+          <h1 className="font-serif text-5xl sm:text-6xl font-bold">TuneBord</h1>
         </header>
 
         {/* メインコンテンツ */}
